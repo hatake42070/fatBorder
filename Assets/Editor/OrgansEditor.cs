b5ed270d9293fba519c4a700c804cef5ef2cdae8
畑山直輝 <naoki42070@gmail.com>
2025-09-14 16:05:24 +0900

    [change]変更の一時保存

--- Assets/Editor/OrgansEditor.cs
+++ Assets/Editor/OrgansEditor.cs
@@ -3,8 +3,26 @@ using UnityEditor;
 using System.Collections.Generic;
 using System.Linq;
 
-public class OrgansEditor
+public class OrgansEditor : EditorWindow
 {
     // --- 変数定義 ---
-    
+    private List<OrganData> allOrgans = new List<OrganData>();
+    private Vector2 scrollPosition;
+
+    [MenuItem("Tools/臓器管理エディタ")]
+    public static void ShowWindow()
+    {
+        GetWindow<OrgansEditor>("臓器管理エディタ");
+    }
+
+    private void OnEnable()
+    {
+        LoadAllOrgans();
+    }
+
+    private void LoadAllOrgans()
+    {
+        allOrgans = EditorUtils.LoadAllAssets<OrganData>();
+    }
+
 }
